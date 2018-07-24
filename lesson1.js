31512b170eea97b6f003b75534504441a34fe051
Milo MacPhail <milomacphail@gmail.com>
2018-07-24 17:27:55 +0000

    Lesson one.a

--- lesson1.js
+++ lesson1.js
@@ -2,3 +2,11 @@ var todos = ["item 1", "item 2", "item 3"]
 
 console.log(todos)
 
+todos.push("item 4")
+todos.push("item 5")
+
+todos[0] = "item 1 updated"
+
+todos.splice(0);
+
+todos.splice(0, 3);
