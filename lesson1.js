1fba71850c448b17798a90b6a06ed9535a30673a
Milo MacPhail <milomacphail@gmail.com>
2018-07-25 19:19:45 +0000

    2nd Lesson

--- lesson1.js
+++ lesson1.js
@@ -7,6 +7,23 @@ todos.push("item 5")
 
 todos[0] = "item 1 updated"
 
-todos.splice(0);
+todos.splice(0, 1);
 
 todos.splice(0, 3);
+
+function displayTodos {
+    console.log("My Todos:", todos);
+}
+
+function changeTodo(position, newValue ) {
+todos[position] = newValue;
+displayTodos(); 
+}
+
+function deleteTodo(position) {
+	todos.splice(position, 1)
+	displayTodos();
+}
+
+
+
