c7b4a2f66751c7fa5080006bf0f4548016507643
Milo MacPhail <milomacphail@gmail.com>
2018-07-26 19:25:41 +0000

    Lesson 3

--- lesson1.js
+++ lesson1.js
@@ -1,29 +1,17 @@
-var todos = ["item 1", "item 2", "item 3"]
-
-console.log(todos)
-
-todos.push("item 4")
-todos.push("item 5")
-
-todos[0] = "item 1 updated"
-
-todos.splice(0, 1);
-
-todos.splice(0, 3);
-
-function displayTodos {
-    console.log("My Todos:", todos);
-}
-
-function changeTodo(position, newValue ) {
-todos[position] = newValue;
-displayTodos(); 
-}
-
-function deleteTodo(position) {
-	todos.splice(position, 1)
-	displayTodos();
-}
-
+var todoList = { 
+  todos: ['item 1', 'item 2', 'item 3'],
+  displayTodos: function() {
+    console.log("My todos", this.todos);
+  }, addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  }, changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos(); 
+  },deleteTodo: function(position) {
+	this.todos.splice(position, 1)
+	this.displayTodos();
+  }
+};
 
 
