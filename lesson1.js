509f57d38016cfd0381a0d58b5a9ab9d627fd9dd
Milo MacPhail <milomacphail@gmail.com>
2018-08-01 04:34:22 +0000

    Lesson 7

--- lesson1.js
+++ lesson1.js
@@ -1,23 +1,38 @@
-var todoList = { 
+var todoList = {
   todos: [],
-  displayTodos: function() {
-    console.log("My todos", this.todos);
-  }, addTodo: function(todoText) {
+  addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos();
-  }, changeTodo: function(position, todoText) {
+  },
+  changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
-    this.displayTodos(); 
-  },deleteTodo: function(position) {
-	this.todos.splice(position, 1)
-	this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1)
   },
   toggleCompleted: function(position) {
     var todo = this.todo[position];
     todo.completed = !todo.completed;
-    this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
   }
 };
